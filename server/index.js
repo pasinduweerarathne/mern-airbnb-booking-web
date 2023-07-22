df8989c01b70ab546ad2e72fa9d9609cca5fcb72
pasindu <pasiweerarathne98@gmail.com>
2023-07-22 20:42:32 +0530

    changed base url backend

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
-    origin: ["https://deploy-mern-"],
+    origin: ["https://vercel.com/pasinduweerarathne/mern-airbnb-booking-web"],
     credentials: true,
   })
 );
