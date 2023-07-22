48fd63d98c1de5091fb70c7b1ba1778ad1d1afe6
pasindu <pasiweerarathne98@gmail.com>
2023-07-22 21:02:18 +0530

    add url

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
-    origin: ["https://vercel.com/pasinduweerarathne/mern-airbnb-booking-web"],
+    origin: ["https://mern-airbnb-booking-web.vercel.app"],
     credentials: true,
   })
 );
